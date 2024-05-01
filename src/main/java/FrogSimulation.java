6ccb23f6371f2b07d436db55a54838617c567378
hyunjaeoh7 <143122764+hyunjaeoh7@users.noreply.github.com>
2024-04-30 22:10:27 -0700

    Update FrogSimulation.java

--- src/main/java/FrogSimulation.java
+++ src/main/java/FrogSimulation.java
@@ -15,14 +15,27 @@ public class FrogSimulation
 	
 	public boolean simulate()
 	{ 
-		/* to be implemented in part (a) */
-
+		int pos = 0;
+		for(int i = 0; i < maxHops; i++){
+			pos += hopDistance();
+			if(pos < 0){
+				return false;
+			else if(pos > goalDistance){
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public double runSimulations(int num)
 	{ 
-		/* to be implemented in part (b) */ 
-
+		double ok = 0;
+		for(int i = 0; i < num; i++){
+			if(simulate()){
+				ok++;
+			}
+		}
+		return ok/(double)num;
 	}
 	
 	
